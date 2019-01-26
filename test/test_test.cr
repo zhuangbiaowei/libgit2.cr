0bfa50ba3e023d0f52d29899c7e6babeedacebb6
zhuangbiaowei <zhuangbiaowei@gmail.com>
2019-01-26 17:20:45 +0800

    remove test code

--- test/test_test.cr
+++
@@ -1,7 +0,0 @@
-require "minitest/autorun"
-
-class TestTest < Minitest::Test
-  def test_one_is_one
-    assert_equal "one", "one"
-  end
-end
--- test/test_test2.cr
+++
@@ -1,7 +0,0 @@
-require "minitest/autorun"
-
-class TestTest2 < Minitest::Test
-  def test_a
-    assert_equal "two", "two"
-  end
-end
